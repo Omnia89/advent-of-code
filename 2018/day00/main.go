d5d9bdf4a6aa8c0a51d1ad58d5189632a07e86ee
Flavio <flavio.cristoforetti@carrier.com>
2026-05-04 11:26:27 +0200

    OLD TIMES - year 2018 - template

---
+++ 2018/day00/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"advent2018/util"
+)
+
+func main() {
+	data := util.GetDataByRow("day03")
+	// data := util.GetTestByRow("day03")
+
+	part1(data)
+	part2(data)
+}
+
+func part1(data []string) {
+	counter := 0
+
+	fmt.Printf("Part 1: %d\n", counter)
+}
+
+func part2(data []string) {
+	counter := 0
+
+	fmt.Printf("Part 2: %d\n", counter)
+}
